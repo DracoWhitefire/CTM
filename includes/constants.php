0e1bc8d78a7aae7b4339517691af788b26eb7fe2
Draco Whitefire <draco@dracowhitefire.net>
2013-05-31 01:43:42 +0200

    Excluded constants.php from depository

--- includes/constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-//General
-	define("SITE_TILE", "Community Ticket Management");
-//MySQL
-	define("DB_SERVER", "localhost");
-	define("DB_USER", "root");
-	define("DB_PW", "");
-	define("DB_NAME", "ctm");
-?>
