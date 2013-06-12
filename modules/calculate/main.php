6aa23b2b279ac8cc4efb42d3e49957822cce685e
Draco Whitefire <draco@dracowhitefire.net>
2013-06-12 13:07:04 +0200

    deletes placeholder content in calculate/main.php

--- modules/calculate/main.php
+++ modules/calculate/main.php
@@ -1,6 +1,4 @@
 
 <?php
-setlocale(LC_TIME, '');
-echo strftime("%b %d %Y %H:%M:%S", mktime(20, 0, 0, 12, 31, 98)) . "\n";
-echo gmstrftime("%b %d %Y %H:%M:%S", mktime(20, 0, 0, 12, 31, 98)) . "\n";
+
 ?>
