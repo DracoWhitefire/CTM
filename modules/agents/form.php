622b60418537af1bfcc6bd024162da94138fec55
Draco Whitefire <draco@dracowhitefire.net>
2013-06-12 16:15:43 +0200

    renames elements in agent form and css

--- modules/agents/form.php
+++ modules/agents/form.php
@@ -1,7 +1,11 @@
 <?php
 // New Agent Form
-	$agentForm = "New Agent Form";
-	$agentForm .= "<div id=\"newAgent_div\"><form id=\"newAgent_form\" method=\"POST\" action=\"index.php" . htmlspecialchars("?id={$current_id}") . "\">";
+	if($editAgent == TRUE) {
+		$agent_set = get_agents($_POST["agent_id"]);
+		$agent = mysql_fetch_array($agent_set);
+	}
+	$agentForm = "Agent Form";
+	$agentForm .= "<div id=\"agentForm_div\"><form id=\"agentForm_form\" method=\"POST\" action=\"index.php" . htmlspecialchars("?id={$current_id}") . "\">";
 	$agentForm .= "<label for=\"forumName_input\">Forum Name</label><input type=\"text\" id=\"forumName_input\" name=\"forumName_input\" /><br />";
 	$agentForm .= "<label for=\"firstName_input\">First Name</label><input type=\"text\" id=\"firstName_input\" name=\"firstName_input\" /><br />";
 	$agentForm .= "<label for=\"lastName_input\">Last Name</label><input type=\"text\" id=\"lastName_input\" name=\"lastName_input\" /><br />";
@@ -11,7 +15,7 @@
 	$agentForm .= "<option value=\"100\">Superadmin</option>";
 	$agentForm .= "</select><br />";
 	$agentForm .= "<label for=\"active_input\">Active</label><input type=\"checkbox\" id=\"active_input\" name=\"active_input\" /><br />";
-	$agentForm .= "<div id=\"newAgentSchedule_div\"><table id=\"newAgentSchedule_table\">";
+	$agentForm .= "<div id=\"agentFormSchedule_div\"><table id=\"agentFormSchedule_table\">";
 	$agentForm .= "<thead><tr><th>Weekday</th><th>Begin Shift</th><th>End Shift</th></tr></thead>";
 	$agentForm .= "<tbody><tr><td>Monday</td><td><input type=\"text\" name=\"mondayBegin_input\" /></td><td><input type=\"text\" name=\"mondayEnd_input\" /></td></tr>";
 	$agentForm .= "<tr><td>Tuesday</td><td><input type=\"text\" name=\"tuesdayBegin_input\" /></td><td><input type=\"text\" name=\"mondayEnd_input\" /></td></tr>";
