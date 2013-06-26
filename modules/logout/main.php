58ae9bf1e147465fc0270fae4f84b8ed4d5c0108
Draco Whitefire <draco@dracowhitefire.net>
2013-06-26 14:26:55 +0200

    adds logout

---
+++ modules/logout/main.php
@@ -0,0 +1,9 @@
+<?php
+	if(isset($_POST["logout_submit"])) {
+		unset($_SESSION["id"]);
+		header("location:index.php");
+	}
+?>
+<form method="POST" id="logout_form" action="index.php?id=5">
+	<input type="submit" id="logout_submit" name="logout_submit" value="Logout" />
+</form>
