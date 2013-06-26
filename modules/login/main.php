651a87a9ab086f770fdc2b687e39d5f9d6c83532
Draco Whitefire <draco@dracowhitefire.net>
2013-06-26 13:41:33 +0200

    adds login logic to login/main.php

--- modules/login/main.php
+++ modules/login/main.php
@@ -3,14 +3,32 @@
 	require_once("includes/functions.php");
 	if(isset($_POST["login"])) {
 		//login attempt
-		$username = trim(mysql_prep($_POST["username_input"]));
-		$password = trim(mysql_prep($_POST["password_input"]));
+		$username = trim(mysqli_prep($_POST["username_input"]));
+		$password = trim(mysqli_prep($_POST["password_input"]));
 		$checkReq_array = array("username_input", "password_input");
 		form_val_required($checkReq_array);
-		$checkLen_array = array("username_input" => "5-30", "password_input" => "5-30");
-		form_val_length($checkLen_array);
+		if(empty($errors)) {
+			$query = "SELECT `user_name`, `passwordhash` FROM `agents`";
+			$agent_set = mysqli_query($connection, $query);
+			while($agent_row = mysqli_fetch_assoc($agent_set)) {
+				if($agent_row["user_name"] == $username) {
+					if(pw_check($password, $agent_row["passwordhash"])) {
+						$message = "SUCCES!";
+					} else {
+						$message = "Login Failed!";
+					}
+					break;
+				} else {
+					$message = "User not found";
+				}
+			}
+			mysqli_free_result($agent_set);
+		} else {
+			$message = "Some fields are not filled in";
+		}
 	} else {
 		//no login attempt
+		$message = "Please enter your user name and password.";
 		$username = "";
 		$password = "";
 		if(isset($_POST["cancel"])) {
@@ -21,6 +39,7 @@
 ?>
 
 <div id="login_div">
+	<?php echo $message; ?>
 	<form id="login_form" method="POST" action="index.php?id=<?php echo $current_id; ?>">
 		<label for="username_input">Username</label> <input type="text" name="username_input" id="username_input" maxlength="30" value="<?php echo htmlentities($username); ?>" /><br />
 		<label for="password_input">Password</label> <input type="password" name="password_input" id="password_input" maxlength="30" value="<?php echo htmlentities($password); ?>" /><br />
