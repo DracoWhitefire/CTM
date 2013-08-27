d3d18fd4a6a3c093b1b4c3c8f797b14e86804ced
Draco Whitefire <draco@dracowhitefire.net>
2013-08-27 10:33:57 +0200

    implements working column filter in user list

--- public/modules/users/list.php
+++ public/modules/users/list.php
@@ -1,31 +1,93 @@
 <?php
-	print_r($_POST);
-	if(isset($_POST["userNameVis_check"])) {
-		setcookie("userNameVis", TRUE, 60*60*24*31);
-		$userNameVis = TRUE;
-	} else {
-		unset($_COOKIE["userNameVis"]);
-		$userNameVis = FALSE;
+	
+	//print_r($_POST);
+	//print_r($_COOKIE);
+	$columnName_array = array("userName", "firstName", "lastName", "forumName", "rank", "active");
+	function listFilter($columnName_array) {
+		$columnFilter_array = array();
+		$expiration = time()+60*60*24*31;
+		foreach($columnName_array as $columnName) {
+			$columnName;
+			$columnFilter_array[$columnName] = FALSE;
+			if(isset($_POST["userListFilter_submit"])) {
+				if(isset($_POST[$columnName . "Vis_check"])) {
+					if($_POST[$columnName . "Vis_check"] == "on") {
+						setcookie($columnName, TRUE, $expiration);
+						$columnFilter_array[$columnName] = TRUE;
+					}
+				} else {
+					unset($_COOKIE[$columnName]);
+				}
+			} else {
+				if(isset($_COOKIE[$columnName])) {
+					if($_COOKIE[$columnName] == TRUE) {
+						$columnFilter_array[$columnName] = TRUE;
+					}
+				}
+			}
+		}
+		return $columnFilter_array;
 	}
+	$columnFilter_array = listFilter($columnName_array);
 	$userList = "<div id=\"userListFilter_div\" ><form id=\"userlistFilter_form\"  action=\"index.php?id=" . urlencode($current_id) . "\" method=\"POST\" >";
 	$userList .= "<div id=\"listColumnFilter_div\" >";
 	$userList .= "<label for \"userNameVis_check\" >User Name</label><input type=\"checkbox\" id=\"userNameVis_check\" name=\"userNameVis_check\" ";
-	if($userNameVis) {
-		$userList .= "active=\"actove\" ";
+	if($columnFilter_array["userName"]) {
+		$userList .= "checked=\"checked\" ";
 	}
 	$userList .= ">";
-	$userList .= "<label for \"firstNameVis_check\" >First Name</label><input type=\"checkbox\" id=\"firstNameVis_check\" name=\"firstNameVis_check\" >";
-	$userList .= "<label for \"lastNameVis_check\" >Last Name</label><input type=\"checkbox\" id=\"lastNameVis_check\" name=\"lastNameVis_check\" >";
-	$userList .= "<label for \"forumNameVis_check\" >Forum Name</label><input type=\"checkbox\" id=\"forumNameVis_check\" name=\"forumNameVis_check\" >";
-	$userList .= "<label for \"rankVis_check\" >Rank</label><input type=\"checkbox\" id=\"rankVis_check\" name=\"rankVis_check\" >";
-	$userList .= "<label for \"activeVis_check\" >Active</label><input type=\"checkbox\" id=\"activeVis_check\" name=\"activeVis_check\" >";
+	$userList .= "<label for \"firstNameVis_check\" >First Name</label><input type=\"checkbox\" id=\"firstNameVis_check\" name=\"firstNameVis_check\" ";
+	if($columnFilter_array["firstName"]) {
+		$userList .= "checked=\"checked\" ";
+	}
+	$userList .= ">";
+	$userList .= "<label for \"lastNameVis_check\" >Last Name</label><input type=\"checkbox\" id=\"lastNameVis_check\" name=\"lastNameVis_check\" ";
+	if($columnFilter_array["lastName"]) {
+		$userList .= "checked=\"checked\" ";
+	}
+	$userList .= ">";
+	$userList .= "<label for \"forumNameVis_check\" >Forum Name</label><input type=\"checkbox\" id=\"forumNameVis_check\" name=\"forumNameVis_check\" ";
+	if($columnFilter_array["forumName"]) {
+		$userList .= "checked=\"checked\" ";
+	}
+	$userList .= ">";
+	$userList .= "<label for \"rankVis_check\" >Rank</label><input type=\"checkbox\" id=\"rankVis_check\" name=\"rankVis_check\" ";
+	if($columnFilter_array["rank"]) {
+		$userList .= "checked=\"checked\" ";
+	}
+	$userList .= ">";
+	$userList .= "<label for \"activeVis_check\" >Active</label><input type=\"checkbox\" id=\"activeVis_check\" name=\"activeVis_check\" ";
+	if($columnFilter_array["active"]) {
+		$userList .= "checked=\"checked\" ";
+	}
+	$userList .= ">";
 	$userList .= "</div>";
-	$userList .= "<input type=\"submit\" id=\"userListFilter_submit\" value=\"Filter\" >";
+	$userList .= "<input type=\"submit\" id=\"userListFilter_submit\" name=\"userListFilter_submit\" value=\"Filter\" >";
 	$userList .= "</div>";
+	
 	$userList .= "<div id=\"userlist_div\"><form id=\"userlist_form\"  action=\"index.php?id=" . urlencode($current_id) . "\" method=\"POST\" >";
 	$userList .= "<table id=\"userlist_table\">";
 	$userList .= "<thead>";
-	$userList .= "<tr><th></th><th>CTM Username</th><th>First Name</th><th>Last Name</th><th>Forum Name</th><th>CTM Rank</th><th>Active</th><th></th></tr>";
+	$userList .= "<tr><th></th>";
+	if($columnFilter_array["userName"]) {
+		$userList .= "<th>CTM Username</th>";
+	}
+	if($columnFilter_array["firstName"]) {
+		$userList .= "<th>First Name</th>";
+	}
+	if($columnFilter_array["lastName"]) {
+		$userList .= "<th>Last Name</th>";
+	}
+	if($columnFilter_array["forumName"]) {
+		$userList .= "<th>Forum Name</th>";
+	}
+	if($columnFilter_array["rank"]) {
+		$userList .= "<th>CTM Rank</th>";
+	}
+	if($columnFilter_array["active"]) {
+		$userList .= "<th>Active</th>";
+	}
+	$userList .= "<th></th></tr>";
 	$userList .= "</thead>";
 	$userList .= "<tbody>";
 	foreach($users_array as $user) {
@@ -44,99 +106,111 @@
 			$userList .= "<input type=\"checkbox\" name=\"" . htmlspecialchars("edit_{$id}") . "\" />";
 		}
 		$userList .= "</td>";
-		$userList .= "<td class=\"name";
-		if(isset($validator->errors["user_name_{$id}"])) {
-			$userList .= " error\"";
-		} else {
-			$userList .= "\"";
+		if($columnFilter_array["userName"]) {
+			$userList .= "<td class=\"name";
+			if(isset($validator->errors["user_name_{$id}"])) {
+				$userList .= " error\"";
+			} else {
+				$userList .= "\"";
+			}
+			$userList .= " >";
+			if($editRow == TRUE) {
+				$userList .= "<input type=\"text\" name=\"" . htmlspecialchars("user_name_{$id}") . "\" value=\"";
+			}
+			if(isset($_POST["user_name_{$id}"])) {
+				$userList .= htmlspecialchars($_POST["user_name_{$id}"]);
+			} else {
+				$userList .= htmlspecialchars($user->userName);
+			}		
+			if($editRow == TRUE) {
+				$userList .= "\" />";
+			}
+			$userList .= "</td>";
 		}
-		$userList .= " >";
-		if($editRow == TRUE) {
-			$userList .= "<input type=\"text\" name=\"" . htmlspecialchars("user_name_{$id}") . "\" value=\"";
+		if($columnFilter_array["firstName"]) {
+			$userList .= "<td class=\"name";
+			if(isset($validator->errors["first_name_{$id}"])) {
+				$userList .= " error\"";
+			} else {
+				$userList .= "\"";
+			}
+			$userList .= " >";
+			if($editRow == TRUE) {
+				$userList .= "<input type=\"text\" name=\"" . htmlspecialchars("first_name_{$id}") . "\" value=\"";
+			}
+			if(isset($_POST["first_name_{$id}"])) {
+				$userList .= htmlspecialchars($_POST["first_name_{$id}"]);
+			} else {
+				$userList .= htmlspecialchars($user->firstName);
+			}		
+			if($editRow == TRUE) {
+				$userList .= "\" />";
+			}
+			$userList .= "</td>"; 
 		}
-		if(isset($_POST["user_name_{$id}"])) {
-			$userList .= htmlspecialchars($_POST["user_name_{$id}"]);
-		} else {
-			$userList .= htmlspecialchars($user->userName);
-		}		
-		if($editRow == TRUE) {
-			$userList .= "\" />";
+		if($columnFilter_array["lastName"]) {
+			$userList .= "<td class=\"name";
+			if(isset($validator->errors["last_name_{$id}"])) {
+				$userList .= " error\"";
+			} else {
+				$userList .= "\"";
+			}
+			$userList .= " >";
+			if($editRow == TRUE) {
+				$userList .= "<input type=\"text\" name=\"" . htmlspecialchars("last_name_{$id}") . "\" value=\"";
+			}
+			if(isset($_POST["last_name_{$id}"])) {
+				$userList .= htmlspecialchars($_POST["last_name_{$id}"]);
+			} else {
+				$userList .= htmlspecialchars($user->lastName);
+			}
+			if($editRow == TRUE) {
+				$userList .= "\" />";
+			}
+			$userList .= "</td>";
 		}
-		$userList .= "</td>";
-		$userList .= "<td class=\"name";
-		if(isset($validator->errors["first_name_{$id}"])) {
-			$userList .= " error\"";
-		} else {
-			$userList .= "\"";
+		if($columnFilter_array["forumName"]) {
+			$userList .= "<td class=\"name";
+			if(isset($validator->errors["forum_name_{$id}"])) {
+				$userList .= " error\"";
+			} else {
+				$userList .= "\"";
+			}
+			$userList .= ">";
+			if($editRow == TRUE) {
+				$userList .= "<input type=\"text\" name=\"" . htmlspecialchars("forum_name_{$id}") . "\" value=\"";
+			}
+			if(isset($_POST["forum_name_{$id}"])) {
+				$userList .= htmlspecialchars($_POST["forum_name_{$id}"]);
+			} else {
+				$userList .= htmlspecialchars($user->forumName);
+			}
+			if($editRow == TRUE) {
+				$userList .= "\" />";
+			}
+			$userList .= "</td>";
 		}
-		$userList .= " >";
-		if($editRow == TRUE) {
-			$userList .= "<input type=\"text\" name=\"" . htmlspecialchars("first_name_{$id}") . "\" value=\"";
+		if($columnFilter_array["rank"]) {
+			$userList .= "<td class=\"rank\" >";
+			if($editRow == TRUE) {
+				$userList .= Rank::selector($user);
+			} else {
+				$rank = Rank::get($user->rank);
+				$userList .= htmlspecialchars($rank->name);
+			}
+			$userList .= "</td>";
 		}
-		if(isset($_POST["first_name_{$id}"])) {
-			$userList .= htmlspecialchars($_POST["first_name_{$id}"]);
-		} else {
-			$userList .= htmlspecialchars($user->firstName);
-		}		
-		if($editRow == TRUE) {
-			$userList .= "\" />";
+		if($columnFilter_array["active"]) {
+			$userList .= "<td><input type=\"checkbox\" name=\"" . htmlspecialchars("active_check_{$id}") . "\" ";
+			if($editRow == FALSE) {
+				$userList .= "disabled=\"disabled\" ";
+			}
+			if($user->active == TRUE) {
+				$userList .= "checked=\"checked\" ";
+			}
+			$userList .= "/>";
+			$userList .= "</td>";
 		}
-		$userList .= "</td>"; 
-		$userList .= "<td class=\"name";
-		if(isset($validator->errors["last_name_{$id}"])) {
-			$userList .= " error\"";
-		} else {
-			$userList .= "\"";
-		}
-		$userList .= " >";
-		if($editRow == TRUE) {
-			$userList .= "<input type=\"text\" name=\"" . htmlspecialchars("last_name_{$id}") . "\" value=\"";
-		}
-		if(isset($_POST["last_name_{$id}"])) {
-			$userList .= htmlspecialchars($_POST["last_name_{$id}"]);
-		} else {
-			$userList .= htmlspecialchars($user->lastName);
-		}
-		if($editRow == TRUE) {
-			$userList .= "\" />";
-		}
-		$userList .= "</td>"; 
-		$userList .= "<td class=\"name";
-		if(isset($validator->errors["forum_name_{$id}"])) {
-			$userList .= " error\"";
-		} else {
-			$userList .= "\"";
-		}
-		$userList .= ">";
-		if($editRow == TRUE) {
-			$userList .= "<input type=\"text\" name=\"" . htmlspecialchars("forum_name_{$id}") . "\" value=\"";
-		}
-		if(isset($_POST["forum_name_{$id}"])) {
-			$userList .= htmlspecialchars($_POST["forum_name_{$id}"]);
-		} else {
-			$userList .= htmlspecialchars($user->forumName);
-		}
-		if($editRow == TRUE) {
-			$userList .= "\" />";
-		}
-		$userList .= "</td>";
-		$userList .= "<td class=\"rank\" >";
-		if($editRow == TRUE) {
-			$userList .= Rank::selector($user);
-		} else {
-			$rank = Rank::get($user->rank);
-			$userList .= htmlspecialchars($rank->name);
-		}
-		$userList .= "</td>"; 
-		$userList .= "<td><input type=\"checkbox\" name=\"" . htmlspecialchars("active_check_{$id}") . "\" ";
-		if($editRow == FALSE) {
-			$userList .= "disabled=\"disabled\" ";
-		}
-		if($user->active == TRUE) {
-			$userList .= "checked=\"checked\" ";
-		}
-		$userList .= "/>";
-		$userList .= "</td>";
 		$userList .= "<td>";
 		if($editList == FALSE) {
 			$userList .= "<button type=\"submit\" name=\"singleEditList\" formmethod=\"post\" value=\"" . htmlspecialchars($id) . "\"  >Edit</button>";
