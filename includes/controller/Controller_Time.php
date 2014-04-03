02c2160bb948331a0ef734dec8686280021e095f
Draco Whitefire <draco@dracowhitefire.net>
2014-04-03 18:32:44 +0200

    changes var names and adds docs to Controller_Time::format()

--- includes/controller/Controller_Time.php
+++ includes/controller/Controller_Time.php
@@ -5,12 +5,19 @@
  */
 Class Controller_Time
 {
-    static function format($time_string, $target) {
+    /**
+     * format
+     * Formats a time string for html output or db entries;
+     * @param string $timeString - the string to be converted
+     * @param string $target - the target of the string
+     * @return string $result - the formatted string
+     */
+    static function format($timeString, $target) {
 	if($target == "db") {
-            $tempresult = preg_replace("/^([0-9]:[0-9]{2})(:[0-9]{2})?$/", "0\\1\\2", $time_string);
-            $result = preg_replace("/^([0-9]{2}:[0-9]{2})$/", "\\1:00", $tempresult);
+            $tempResult = preg_replace("/^([0-9]:[0-9]{2})(:[0-9]{2})?$/", "0\\1\\2", $timeString);
+            $result = preg_replace("/^([0-9]{2}:[0-9]{2})$/", "\\1:00", $tempResult);
 	} elseif ($target == "html") {
-            $result = preg_replace("/^(0)?([1-9]?[0-9]:[0-9]{2})(:[0-9]{2})$/", "\\2", $time_string);			
+            $result = preg_replace("/^(0)?([1-9]?[0-9]:[0-9]{2})(:[0-9]{2})$/", "\\2", $timeString);			
 	}
 	return htmlspecialchars($result);
     }
