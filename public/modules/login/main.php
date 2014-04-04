bb63971ea505234db4a51abb1432ceee3cede525
Draco Whitefire <draco@dracowhitefire.net>
2014-04-04 10:04:33 +0200

    changes indentation in modules/login/main

--- public/modules/login/main.php
+++ public/modules/login/main.php
@@ -1,53 +1,53 @@
 <?php
-	if(isset($_POST["login"])) {
-		//login attempt
-                $db = call_user_func(DB_CLASS . "::getInstance");
-		$username = trim($db->query_prep($_POST["username_input"]));
-		$password = trim($db->query_prep($_POST["password_input"]));
-		$checkReq_array = array("username_input", "password_input");
-		$validator = new Controller_Validator;
-		$validator->required($checkReq_array);
-		if(empty($validator->errors)) {
-			$users = Model_User::get("all");
-                        foreach($users as $user) {
-				if($user->userName == $username) {
-					if($user->pw_check($password)) {
-						$session->login($user);
-						header("location: index.php");
-					} else {
-						$message = "Login Failed!";
-					}
-					break;
-				} else {
-					$message = "User not found";
-				}
-                        } 
-		} else {
-			$message = "Some fields are not filled in";
-		}
-	} else {
-		//no login attempt
-		$message = "Please enter your user name and password.";
-		$username = "";
-		$password = "";
-		if(isset($_POST["cancel"])) {
-			header("location: index.php");
-			exit;
-		}
-	}
+if(isset($_POST["login"])) {
+    //login attempt
+    $db = call_user_func(DB_CLASS . "::getInstance");
+    $username = trim($db->query_prep($_POST["username_input"]));
+    $password = trim($db->query_prep($_POST["password_input"]));
+    $checkReq_array = array("username_input", "password_input");
+    $validator = new Controller_Validator;
+    $validator->required($checkReq_array);
+    if(empty($validator->errors)) {
+        $users = Model_User::get("all");
+        foreach($users as $user) {
+            if($user->userName == $username) {
+                if($user->pw_check($password)) {
+                    $session->login($user);
+                    header("location: index.php");
+                } else {
+                    $message = "Login Failed!";
+                }
+                break;
+            } else {
+                $message = "User not found";
+            }
+        } 
+    } else {
+        $message = "Some fields are not filled in";
+    }
+} else {
+    //no login attempt
+    $message = "Please enter your user name and password.";
+    $username = "";
+    $password = "";
+    if(isset($_POST["cancel"])) {
+        header("location: index.php");
+        exit;
+    }
+}
 ?>
 
 <div id="login_div">
-	<?php echo $message; ?>
-	<form id="login_form" method="POST" action="index.php?id=<?php echo $currentId; ?>">
-		<label for="username_input">Username</label> <input type="text" name="username_input" id="username_input" maxlength="30" value="<?php echo htmlentities($username); ?>" /><br />
-		<label for="password_input">Password</label> <input type="password" name="password_input" id="password_input" maxlength="30" value="<?php echo htmlentities($password); ?>" /><br />
-		<div id="loginButtons_div">
-			<input type="submit" name="login" value="Log In" /><input type="submit" name="cancel" value="Cancel" />
-		</div>
-	</form>
+    <?php echo $message; ?>
+    <form id="login_form" method="POST" action="index.php?id=<?php echo $currentId; ?>">
+        <label for="username_input">Username</label> <input type="text" name="username_input" id="username_input" maxlength="30" value="<?php echo htmlentities($username); ?>" /><br />
+        <label for="password_input">Password</label> <input type="password" name="password_input" id="password_input" maxlength="30" value="<?php echo htmlentities($password); ?>" /><br />
+        <div id="loginButtons_div">
+            <input type="submit" name="login" value="Log In" /><input type="submit" name="cancel" value="Cancel" />
+        </div>
+    </form>
 </div>
 
 <?php
-	//print_r($_POST);
-	//print_r($errors);
+    //print_r($_POST);
+    //print_r($errors);
