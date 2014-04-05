842b9c8e4e9ca8792270891456511a24f5971c86
Draco Whitefire <draco@dracowhitefire.net>
2014-04-05 23:59:24 +0200

    extracts _getByValue() from Model_Rank::get()

--- includes/model/Model_Rank.php
+++ includes/model/Model_Rank.php
@@ -19,20 +19,10 @@ class Model_Rank extends Model_Dao
      * @return object|array - instance of Model_Rank or array thereof
      */
     public static function get($selection = "all") {
-        $db = call_user_func(DB_CLASS . "::getInstance");
-        $query  = "SELECT * ";
-        $query .= "FROM `ranks` ";
         if($selection == "all") {
             return self::_getAll();
         } elseif(is_numeric($selection)) {	
-            if((1 <= $selection) && ($selection <= self::_getMax())) {
-                $selection = $db->query_prep($selection);
-            } else {
-                $selection = 1;
-            }
-            $query .= "WHERE `value` = {$selection} ";
-            $query .= "LIMIT 1 ";
-            return self::get_by_query($query);
+            return self::_getByValue($selection);
         }
     }
     
@@ -61,6 +51,26 @@ class Model_Rank extends Model_Dao
     private static function _getAll() {
         $query  = "SELECT * ";
         $query .= "FROM `ranks` ";
+        return self::get_by_query($query);
+    }
+    
+    /**
+     * _getByValue
+     * Returns single Model_Rank based on $value
+     * @param int $value - the value for the rank
+     * @return Model_Rank - instance of Model_Rank
+     */
+    private static function _getByValue($value) {
+        $db = call_user_func(DB_CLASS . "::getInstance");
+        if((1 <= $value) && ($value <= self::_getMax())) {
+            $value = $db->query_prep($value);
+        } else {
+            $value = 1;
+        }
+        $query  = "SELECT * ";
+        $query .= "FROM `ranks` ";
+        $query .= "WHERE `value` = {$value} ";
+        $query .= "LIMIT 1 ";
         return self::get_by_query($query);
     }
 }
