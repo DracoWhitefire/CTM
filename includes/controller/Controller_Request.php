1a010040fa076002b678c4e817e212bb701bc698
Draco Whitefire <draco@dracowhitefire.net>
2014-04-07 14:37:39 +0200

    adds Controller_Request

--- includes/controller/Controller_Request.php
+++ includes/controller/Controller_Request.php
@@ -4,9 +4,10 @@
  */
 class Controller_Request
 {
-    private static $_getStore = [];
-    private static $_postStore = [];
-    private static $_sessionStore = [];
+    private static $_getStore;
+    private static $_postStore;
+    private static $_cookieStore;
+    private static $_sessionStore;
     
     /**
      * prepares string for output/database
@@ -18,24 +19,66 @@ class Controller_Request
     }
     
     /**
-     * Handler method to filter requests;
-     * @param string $requestType - GET, POST or SESSION
-     * @param string $keyName - the specific request key
-     * @return string|bool - the filtered value for request key or bool false if not available
+     * Handler to filter GET requests;
+     * @param string $keyName - the _GET key
+     * @return string - the filtered value for $keyName
      */
-    public static function filter($requestType, $keyName) {
-        $attribName = "_" . strtolower($requestType) . "Store";
-        $requestName = "_" . $requestType;
-//        $foo = "bar";
-//        $requestName = "foo";
-        print_r($_GET);                 // this works
-        print_r(${$requestName});       // this does not
-        echo ${$requestName};
-        if(!isset(self::${$attribName})) {
-            foreach(${$requestName} as $key => $value) {
-                static::${$attribName}[$key] = self::_cleanup($value);
+    public static function get($keyName) {
+        if(!isset(static::$_getStore)) {
+            foreach($_GET as $key => $value) {
+                static::$_getStore[$key] = self::_cleanup($value);
             }
         }
-        return isset(self::${$attribName}[$keyName]) ? self::${$attribName}[$keyName] : false;
+        return isset(static::$_getStore[$keyName]) ? static::$_getStore[$keyName] : false;
     }
+    
+    /**
+     * Handler to filter POST requests;
+     * @param string $keyName - the _POST key
+     * @return string - the filtered value for $keyName
+     */
+    public static function post($keyName) {
+        if(!isset(static::$_postStore)) {
+            foreach($_POST as $key => $value) {
+                static::$_postStore[$key] = self::_cleanup($value);
+            }
+        }
+        return isset(static::$_postStore[$keyName]) ? static::$_postStore[$keyName] : false;
+    }
+    
+    /**
+     * Handler to filter COOKIE requests;
+     * @param string $keyName - the _COOKIE key
+     * @return string - the filtered value for $keyName
+     */
+    public static function cookie($keyName) {
+        if(!isset(static::$_cookieStore)) {
+            foreach($_COOKIE as $key => $value) {
+                static::$_cookieStore[$key] = self::_cleanup($value);
+            }
+        }
+        return isset(static::$_cookieStore[$keyName]) ? static::$_cookieStore[$keyName] : false;
+    }
+    
+    /**
+     * Handler to filter SESSION requests;
+     * @param string $keyName - the _SESSION key
+     * @return string|array - the filtered value(s) for $keyName
+     */
+    public static function session($keyName) {
+        if(!isset(static::$_sessionStore)) {
+            foreach($_SESSION as $key => $value) {
+                if(is_string($value)) {
+                    static::$_sessionStore[$key] = self::_cleanup($value);
+                } elseif(is_array($value)) {
+                    foreach($value as $valueKey => $valueValue) {
+                        $value[$valueKey] = self::_cleanup($valueValue);
+                    }
+                }
+                
+            }
+        }
+        return isset(static::$_sessionStore[$keyName]) ? static::$_sessionStore[$keyName] : false;
+    }
+   
 }
