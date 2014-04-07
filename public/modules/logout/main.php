f09ed7f2d361040af5ebc129e9a415efca638b41
Draco Whitefire <draco@dracowhitefire.net>
2014-04-07 11:38:12 +0200

    renames css classes and ids in modules/logout/main.php

--- public/modules/logout/main.php
+++ public/modules/logout/main.php
@@ -1,8 +1,8 @@
 <?php
-    if(isset($_POST["logout_submit"])) {
+    if(isset($_POST["logoutSubmit"])) {
         $session->logout();
     }
 ?>
-<form method="POST" id="logout_form" action="index.php?id=<?php echo LOGOUT_MODULE_ID; ?>">
-    <input type="submit" id="logout_submit" name="logout_submit" value="Logout" />
+<form method="POST" id="logoutForm" action="index.php?id=<?php echo LOGOUT_MODULE_ID; ?>">
+    <input type="submit" id="logoutSubmit" name="logoutSubmit" value="Logout" />
 </form>
