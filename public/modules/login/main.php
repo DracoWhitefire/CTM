09c83176534859ec3b274e8ba7cd0ba61c5287a4
Draco Whitefire <draco@dracowhitefire.net>
2014-04-07 11:16:33 +0200

    renames css classes and ids in modules/login/main.php

--- public/modules/login/main.php
+++ public/modules/login/main.php
@@ -1,9 +1,9 @@
 <?php
 if(isset($_POST["login"])) {
     //login attempt
-    $username = call_user_func(DB_CLASS . "::queryPrep", $_POST["username_input"]);
-    $password = call_user_func(DB_CLASS . "::queryPrep", $_POST["password_input"]);
-    $checkReq_array = array("username_input", "password_input");
+    $username = call_user_func(DB_CLASS . "::queryPrep", $_POST["username"]);
+    $password = call_user_func(DB_CLASS . "::queryPrep", $_POST["password"]);
+    $checkReq_array = array("username", "password");
     $validator = new Controller_Validator;
     $validator->required($checkReq_array);
     if(empty($validator->errors)) {
@@ -38,10 +38,10 @@ if(isset($_POST["login"])) {
 
 <div id="login_div">
     <?php echo $message; ?>
-    <form id="login_form" method="POST" action="index.php?id=<?php echo $currentId; ?>">
-        <label for="username_input">Username</label> <input type="text" name="username_input" id="username_input" maxlength="30" value="<?php echo htmlentities($username); ?>" /><br />
-        <label for="password_input">Password</label> <input type="password" name="password_input" id="password_input" maxlength="30" value="<?php echo htmlentities($password); ?>" /><br />
-        <div id="loginButtons_div">
+    <form id="loginForm" method="POST" action="index.php?id=<?php echo $currentId; ?>">
+        <label for="username">Username</label> <input type="text" name="username" id="username" maxlength="30" value="<?php echo htmlentities($username); ?>" /><br />
+        <label for="password">Password</label> <input type="password" name="password" id="password" maxlength="30" value="<?php echo htmlentities($password); ?>" /><br />
+        <div id="loginButtons">
             <input type="submit" name="login" value="Log In" /><input type="submit" name="cancel" value="Cancel" />
         </div>
     </form>
